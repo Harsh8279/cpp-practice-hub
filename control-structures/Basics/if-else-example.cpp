9f26fedef6fbab0ee8d16cdefb62b875342c3c36
Harsh Patel <65580408+Harsh8279@users.noreply.github.com>
2024-12-01 09:34:15 +0000

    added concept of else if statement :: HP

--- control-structures/Basics/if-else-example.cpp
+++ control-structures/Basics/if-else-example.cpp
@@ -24,6 +24,28 @@ public:
 
 };
 
+class NumberValidator{
+
+private:
+    int num;
+
+public:
+    NumberValidator(int n): num(n){};
+
+    void get_status_of_number(){
+        if(num>0){
+            cout<<num<<" is Positive Number"<<endl;
+        }
+        else if(num<0){
+            cout<<num<<" is Negative Number"<<endl;
+        }
+        else{
+            cout<<num<<" is Zero!!"<<endl;
+        }
+    }
+
+};
+
 int main(){
 
     AgeValidator a1 = AgeValidator(12);
@@ -32,4 +54,11 @@ int main(){
     AgeValidator a2 = AgeValidator(19);
     a2.is_valid();
 
+    NumberValidator n1 = NumberValidator(5);
+    NumberValidator n2 = NumberValidator(-10);
+    NumberValidator n3 = NumberValidator(0);
+
+    n1.get_status_of_number();
+    n2.get_status_of_number();
+    n3.get_status_of_number();
 }
