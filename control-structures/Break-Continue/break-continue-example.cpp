b34b8302c05245065c9e1e2226b0ad9e05a4a27a
Harsh Patel <65580408+Harsh8279@users.noreply.github.com>
2024-12-02 01:32:50 +0000

    added continue statement :: HP

--- control-structures/Break-Continue/break-continue-example.cpp
+++ control-structures/Break-Continue/break-continue-example.cpp
@@ -22,7 +22,36 @@ public:
 
 };
 
+class ContinueExample{
+
+private:
+    int num;
+
+public:
+
+    ContinueExample(int n):num(n){};
+
+    void status_check(){
+        while(num>0){
+
+            num++;
+            if(num>6 && num<10){
+                continue;
+            }
+            cout<<"Num : "<<num<<endl;
+
+            if(num>13){
+                break;
+            }
+        }
+        cout<<"\n---------------------------------\n";
+    }
+
+};
+
 int main(){
+
+    cout<<"\n\n-------------------Break Part-------------------\n";
 
     BreakExample b1 = BreakExample(6);
     BreakExample b2 = BreakExample(1);
@@ -33,6 +62,18 @@ int main(){
     b2.status_check();
     b3.status_check();
     b4.status_check();
+
+    cout<<"\n\n-------------------Continue Part-------------------\n";
+
+    ContinueExample c1 = ContinueExample(8);
+    ContinueExample c2 = ContinueExample(10);
+    ContinueExample c3 = ContinueExample(1);
+    ContinueExample c4 = ContinueExample(-5);
+
+    c1.status_check();
+    c2.status_check();
+    c3.status_check();
+    c4.status_check();
 
     return 0;
 
