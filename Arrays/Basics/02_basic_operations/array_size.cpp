afc8b56d0ddbcd8681c7d4ef905ba396673b2ff6
Harsh Patel <65580408+Harsh8279@users.noreply.github.com>
2024-12-08 18:14:24 +0000

    added size of array :: HP

---
+++ Arrays/Basics/02_basic_operations/array_size.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class ArraySize{
+
+private:
+    int data[7] = {1,2,3,4,5,6,7};
+
+public:
+
+    ArraySize(){
+        cout<<"Size of array : "<<sizeof(data)/sizeof(data[0])<<endl;
+    }
+
+};
+
+int main(){
+
+    ArraySize();
+
+    return 0;
+
+}
