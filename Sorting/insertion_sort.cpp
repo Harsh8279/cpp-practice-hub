cc5966be9b468029be2245cf936aee105a095fc3
Harsh Patel <65580408+Harsh8279@users.noreply.github.com>
2024-12-14 21:07:45 +0000

    added insertion sort

---
+++ Sorting/insertion_sort.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class InsertionSort{
+
+private:
+
+    vector<int> arr;
+    int size;
+
+public:
+    InsertionSort(){
+        cout<<"Enter size of The Vector : ";
+        cin>>size;
+
+        cout<<"Enter Unsorted Array : \n";
+        for(int i = 0;i<size;i++){
+            int num;
+            cout<<"Enter "<<i+1<<" Element : ";
+            cin>>num;
+            arr.push_back(num);
+        }
+
+    }
+
+    void perform_sorting(){
+        vector<int> new_vector;
+
+        new_vector.push_back(arr[0]);
+        
+       for(int i = 1;i<size;i++){
+        if(arr[i]>arr[i-1]){
+            new_vector.push_back(arr[i]);
+        }
+        else{
+            int temp = new_vector[i-1];
+            new_vector[i-1] = arr[i];
+            new_vector.push_back(temp);
+        }
+        cout<<"\n------------ Sorted Array ------------\n";
+        for(auto data: new_vector){
+            cout<<data;
+        }
+       }
+        
+    }
+
+};
+
+int main(){
+    InsertionSort in1 = InsertionSort();
+
+    in1.perform_sorting();
+    return 0;
+}
