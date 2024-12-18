8b7c069ee625edf8a214bc9a6077c4b109f6de0d
Harsh Patel <65580408+Harsh8279@users.noreply.github.com>
2024-12-18 00:56:50 +0000

    added explanation for insertion_sort :: HP

--- Sorting/insertion_sort.cpp
+++ Sorting/insertion_sort.cpp
@@ -57,3 +57,40 @@ int main(){
     in1.perform_sorting();
     return 0;
 }
+
+
+/*
+
+            Insertion sort
+
+    Right Shifting and placing right element at right place.
+
+    e.g. 
+    4 1 6 5 3 2
+
+
+    4 1 6 5 3 2
+        new_arr =  4 
+        Round - 1 -> i = 1
+            1 < 4 ? yes -->  right shift 4 and place 1 at 0 th pos
+    4 1 6 5 3 2
+        new_arr =  1 4
+        Round - 2 -> i = 2
+            6 < 4 ? No --> Insert after 4
+    4 1 6 5 3 2
+        new_arr = 1 4 6
+        Round - 3 -> i = 3
+        5 < 6 ? yes --> 5 < 4 ? No --> right shift 6 and place 5 at 2 pos
+    4 1 6 5 3 2
+        new_arr = 1 4 5 6
+        Round - 4 --> i = 4
+        3 < 6 ? yes --> 3 < 5 ? yes --> 3 < 4 ? yes --> 3 < 1 ? No. -->  right shift till 4 and place 3 at 2 Pos
+
+    4 1 6 5 3 2
+        new_arr = 1 3 4 5 6
+        Round - 5 -> i = 5
+        2 < 6 ? yes --> 2 < 5 ? yes --> 2 < 4 ? yes --> 2 < 3 ? yes --> 2 < 1 ? No --> -->  right shift till 3 and place 2 at 1 Pos
+    
+    new_arr = 1 2 3 4 5 6
+
+*/
