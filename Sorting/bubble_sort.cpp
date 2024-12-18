2045ea23fe18758ef211c9ad157d6221464063e1
Harsh Patel <65580408+Harsh8279@users.noreply.github.com>
2024-12-18 00:03:55 +0000

    added comment in bubble_sort :: HP

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -56,3 +56,75 @@ int main(){
     return 0;
 
 }
+
+/*
+            Bubble Sort
+
+    We Will get Largest Element at the end
+
+    Iterate the loop till size-1
+
+    and
+
+    Compare Adjacent Values
+
+    Adjacent means Aajubaju ni value
+
+    e.g. 
+    4 1 6 5 3 2
+
+    1st Iter - i = 0
+        4 1 6 5 3 2 --> j = 0
+        compare 4 > 1? yes --> swap 
+        1 4 6 5 3 2 --> j = 1
+        compare 4 > 6 ? No --> ignore
+        1 4 6 5 3 2 --> j = 2
+        compare 6 > 5 ? yes --> swap
+        1 4 5 6 3 2 --> j = 3
+        compare 6 > 3 ? yes --> swap
+        1 4 5 3 6 2 --> j = 4
+        compare 6 > 2 ? yes --> swap
+        1 4 5 3 2 6 --> j = 5
+        In the 1st Iter we got our highest element at the end of the array.
+    2nd Iter - i = 1
+        1 4 5 3 2 6 --> j = 0
+        compare 1 > 4 ? No --> Ignore
+        1 4 5 3 2 6 --> j = 1
+        compare 4 > 5 ? No --> Ignore
+        1 4 5 3 2 6 --> j = 2
+        compare 5 > 3 ? Yes --> Swap
+        1 4 3 5 2 6 --> j = 3
+        compare 5 > 2 ? Yes --> Swap
+        1 4 3 2 5 6 --> j = 4
+        No Need to compare 5 and 6 because they are largest.
+
+    3rd Iter - i = 2
+        1 4 3 2 5 6 --> j = 0
+        compare 1 > 4 ? No --> Ignore
+        1 4 3 2 5 6 --> j = 1
+        compare 4 > 3 ? Yes --> swap
+        1 3 4 2 5 6 --> j = 2
+        compare 4 > 2 ? Yes --> swap
+        1 3 2 4 5 6 --> j = 3
+        No Need to compare 4 and 5 and 6 because they are largest.
+    
+    4th Iter - i = 3
+        1 3 2 4 5 6 --> j = 0
+        compare 1 > 3 ? No --> Ignore
+        1 3 2 4 5 6 --> j = 1
+        compare 3 > 2 ? Yes --> swap
+        1 2 3 4 5 6 --> j = 2
+        No Need to compare 3, 4 and 5 and 6 because they are largest.
+
+    5th Iter -  i = 4
+        1 2 3 4 5 6 --> j = 0
+        compare 1 > 2 ? No --> Ignore
+        1 2 3 4 5 6 --> j = 1
+        No need to compare 2,3,4,5,6 because they are largest.
+    
+    Now we got our entire array sorted.
+
+
+
+
+*/
